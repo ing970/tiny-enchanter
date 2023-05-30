74c2db6d5c86c0d1e52029b6869911c960b1ecbe
wjco <122499167+wjco@users.noreply.github.com>
2023-05-30 16:42:48 +0900

    Feat: Apply checkpoint and lora
    divide into function units to pull data from the DB
    make logics which change checkpoint or lora models
    
    편리하게 디비에서 데이터를 가져오기 위해서 함수 단위로 나눴다.
    체크포인트 및 로라 모델을 변경하는 로직을 만들었다.

--- test_code/test.py
+++ test_code/test.py
@@ -4,48 +4,59 @@ import io
 import base64
 from PIL import Image, PngImagePlugin
 
-url = "http://127.0.0.1:7860"
-payload = {
-"enable_hr": False,
-"denoising_strength": 0,
-"firstphase_width":0,
-"firstphase_height":0,
-"prompt":"chibi",
-"styles":["string"],
-"seed":-1,
-"subseed":-1,
-"subseed_strength":0,
-"seed_resize_from_h": -1,
-"seed_resize_from_w": -1,
-"batch_size":1,
-"n_iter":1,
-"steps":50,
-"cfg_scale":7,
-"width":512,
-"height":512,
-"restore_faces":False,
-"tiling":False,
-"negative_prompt":"string",
-"eta":0,
-"s_churn":0,
-"s_tmax": 0,
-"s_tmin": 0,
-"s_noise": 1,
-"sampler_index" : "Euler"
-}
-
-response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
-
-r = response.json()
-
-for i in r['images']:
-    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
-
-    png_payload = {
-        "image": "data:image/png;base64," + i
+def load_model(url, checkpoint, pos_prompt, neg_prompt, steps, cfg_scale):
+    change_model(checkpoint)
+    payload = {
+        "enable_hr": False,
+        "denoising_strength": 0,
+        "firstphase_width":0,
+        "firstphase_height":0,
+        "prompt":pos_prompt,
+        "styles":["string"],
+        "seed":-1,
+        "subseed":-1,
+        "subseed_strength":0,
+        "seed_resize_from_h": -1,
+        "seed_resize_from_w": -1,
+        "batch_size":1,
+        "n_iter":1,
+        "steps":steps,
+        "cfg_scale":cfg_scale,
+        "width":512,
+        "height":512,
+        "restore_faces":False,
+        "tiling":False,
+        "negative_prompt":neg_prompt,
+        "eta":0,
+        "s_churn":0,
+        "s_tmax": 0,
+        "s_tmin": 0,
+        "s_noise": 1,
+        "sampler_index" : "Euler"
     }
-    response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+    r = response.json()
+    for i in r['images']:
+        image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+        png_payload = {
+            "image": "data:image/png;base64," + i
+        }
+        response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+        pnginfo = PngImagePlugin.PngInfo()
+        pnginfo.add_text("parameters", response2.json().get("info"))
+        image.save('output.png', pnginfo=pnginfo)
 
-    pnginfo = PngImagePlugin.PngInfo()
-    pnginfo.add_text("parameters", response2.json().get("info"))
-    image.save('output.png', pnginfo=pnginfo)
+def change_model(checkpoint):
+    option_payload = {
+        "sd_model_checkpoint": checkpoint
+    }
+    response = requests.post(url=f'{url}/sdapi/v1/options', json=option_payload)
+
+if __name__ == "__main__":
+    url = "http://127.0.0.1:7860"
+    checkpoint = "qteamixQ_gamma"
+    pos_prompt = "1boy, solo, blue hair, animal ears, chibi, white background, <lora:BlueSD:1>"
+    neg_prompt = "(jpeg artifacts), (blurry), (blurred), (blur), bad anatomy, worst quality, bad quality, disfigured, deformed, malformed, mutant, gross, disgusting, out of frame, monochrome, poorly drawn, extra limbs, extra fingers, missing limbs, misshaped, poorly drawn, mutated hands and fingers, ugly, fat, cubism, text, logo, duplicate, bad eyes, misaligned eyes, deformed mouth, lowres, signature, watermark, username, dots, error, cropped, easynegative"
+    steps = 40
+    cfg_scale = 4
+    load_model(url, checkpoint, pos_prompt, neg_prompt, steps, cfg_scale)
