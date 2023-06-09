0bd36379de9f115c5e8663b3d38a8fdb040c2e9d
wjco <122499167+wjco@users.noreply.github.com>
2023-06-09 15:38:11 +0900

    Feat: Apply FastAPI
    Apply FastAPI tamplate
    
    FastAPI 템플릿을 적용했다

--- test_code/NiNi/connection.py
+++ test_code/NiNi/connection.py
@@ -1,27 +1,34 @@
-from flask import Flask, render_template, request, g
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import json
 import tiny_enchanter as model
 
-app = Flask(__name__)
+app = FastAPI()
+templates = Jinja2Templates(directory="templates") 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.route("/")
-@app.route("/index")
-def home():
-    return render_template('index.html')
+@app.get('/', response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request}) 
 
-@app.route("/choice")
-def choice():
-    return render_template('choice.html')
+@app.get('/choice', response_class=HTMLResponse)
+async def choice(request: Request):
+    return templates.TemplateResponse("choice.html", {"request": request})
 
-@app.route("/result", methods=('POST', 'GET'))
-def result():
-    gender = request.form['gender']
-    animal_ears = request.form['animalears']
-    tail = request.form['tail']
-    hair_style = request.form['hairstyle']
-    hair_ornament = request.form['hairornament']
-    hair_color = request.form['hair-color']
-    eyes_color = request.form['eyes-color']
-    clothes = request.form['clothes']
+@app.post('/result', response_class=HTMLResponse)
+async def result_loading(request: Request):
+    form_data = await request.form()
+    gender = form_data.get('gender')
+    animal_ears = form_data.get('animalears')
+    tail = form_data.get('tail')
+    hair_style = form_data.get('hairstyle')
+    hair_ornament = form_data.get('hairornament')
+    hair_color = form_data.get('haircolor')
+    eyes_color = form_data.get('eyescolor')
+    clothes = form_data.get('clothes')
 
     doc = {
         "00": "00",
@@ -35,23 +42,29 @@ def result():
         "08": clothes
     }
 
-    # images = run_model(doc)
+    cps = model.get_checkpoints()
+    checkpoints = list(cps.keys())
+    lora_prompts = list(cps.values())
 
-    return render_template('show_image.html')
+    return templates.TemplateResponse("result.html", {"request": request, "doc": doc, "checkpoints": checkpoints, "lora_prompts": lora_prompts})
 
-# @app.after_request
-# @after_this_request
+@app.post('/image_generate')
+async def image_generate(request: Request):
+    txts_b = await request.body()
+    txts = list(json.loads(txts_b).values())
 
-def run_model(doc):
     url = "http://127.0.0.1:7860"
-    checkpoints, pos_prompt, neg_prompt, steps, cfg_scale = model.get_data(doc)
-    images = []
+    img_no = txts[0]
+    doc = txts[1]
+    checkpoint = txts[2]
+    pos_prompt, neg_prompt, steps, cfg_scale = model.get_data(doc)
+    pos_prompt += ", " + txts[3]
+    file_name=f'result_{img_no}'
 
-    for checkpoint in checkpoints:
-        i = checkpoints.index(checkpoint) + 1
-        images += model.draw_image(url, checkpoint, pos_prompt, neg_prompt, steps, cfg_scale, file_name=f'result_{i}')
+    model.draw_image(url, checkpoint, pos_prompt, neg_prompt, steps, cfg_scale, file_name=file_name)
 
-    return images
+    # return {file_name: f'{file_name}.png'}
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    uvicorn.run("connection:app", reload=True)
+# uvicorn connection:app --reload
